2e127b7022b1f7ad4da887724da42dc971fdc82b
Petr Loginov <peter30260@gmail.com>
2025-03-15 18:34:09 +0300

    I want to kill myself I hate git so much

--- src/settings.py
+++ src/settings.py
@@ -5,19 +5,19 @@ WIDTH = 800
 HEIGHT = 800
 FPS = 60
 
-bg = pygame.image.load('../resources/images/field.png')
-player = [pygame.image.load('../resources/images/player.png'), pygame.image.load('../resources/images/player2.png')]
-gnome = [pygame.image.load('../resources/images/gnome.png'), pygame.image.load('../resources/images/gnome2.png')]
-beer = pygame.image.load('../resources/images/beer.png')
+bg = pygame.image.load('resources/images/field.png')
+player = [pygame.image.load('resources/images/player.png'), pygame.image.load('resources/images/player2.png')]
+gnome = [pygame.image.load('resources/images/gnome.png'), pygame.image.load('resources/images/gnome2.png')]
+beer = pygame.image.load('resources/images/beer.png')
 
 pygame.font.init()
-font = pygame.font.Font('../resources/fonts/Vezitsa.ttf', 56)
+font = pygame.font.Font('resources/fonts/Vezitsa.ttf', 56)
 
-with open('../resources/high_score.txt', 'r') as file:
+with open('resources/high_score.txt', 'r') as file:
     hs = file.readline()
 
-music_path = '../resources/audio/gnome_music.mp3'
-laugh_path = '../resources/audio/hihi.mp3'
+music_path = 'resources/audio/gnome_music.mp3'
+laugh_path = 'resources/audio/hihi.mp3'
 
 count = 0
 lives = 3
