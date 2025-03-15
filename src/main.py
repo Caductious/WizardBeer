9c6a85a065c6373862f3bf003e96219ad9388718
Petr Loginov <peter30260@gmail.com>
2025-03-15 16:33:08 +0300

    Created sprites

--- src/main.py
+++ src/main.py
@@ -2,7 +2,9 @@ import pygame
 import random
 
 from settings import (WIDTH_SCREEN, WIDTH, HEIGHT, FPS, bg, player,
-                      gnome, beer, font, hs, high_score, laugh_path, music_path)
+                    font, hs, high_score, laugh_path, music_path)
+
+from sprites import Player, Gnome, Projectile
 
 pygame.init()
 pygame.mixer.init()
@@ -15,58 +17,13 @@ music.play(-1)
 game_over = False
 
 
-class Gnome:
-    def __init__(self):
-        self.image = gnome
-        self.width = 128
-        self.height = 128
-        self.walk = 0
-        self.y = 0
-        self.x = random.randrange(WIDTH - self.width)
-        self.speedx = random.randrange(-8, 8)
-        self.speedy = random.randrange(3, 8)
-
-    def draw(self, screen):
-        screen.blit(self.image[self.walk], (self.x, self.y))
 
 
-class Player:
-    def __init__(self):
-        self.image = player[0]
-        self.width = self.image.get_width()
-        self.height = self.image.get_height()
-        self.y = HEIGHT - self.height
-        self.x = WIDTH // 2.5
-        self.speedx = 10
-
-    def movement(self):
-        keystate = pygame.key.get_pressed()
-        if keystate[pygame.K_LEFT]:
-            self.x -= self.speedx
-        if keystate[pygame.K_RIGHT]:
-            self.x += self.speedx
-        if self.x < 0:
-            self.x = 0
-        if self.x > WIDTH - self.width:
-            self.x = WIDTH - self.width
-
-    def draw(self, screen):
-        screen.blit(self.image, (self.x, self.y))
 
 
-class Projectile:
-    def __init__(self):
-        self.image = beer
-        self.x, self.y = player_game.x, player_game.y
-        self.width = self.image.get_width()
-        self.height = self.image.get_height()
-        self.speedy = 10
 
-    def beer_cast(self):
-        self.y -= self.speedy
 
-    def draw(self, screen):
-        screen.blit(self.image, (self.x, self.y))
+
 
 
 def render():
@@ -140,7 +97,7 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
-                projectiles.append(Projectile())
+                projectiles.append(Projectile(player_game.x, player_game.y))
                 player_game.image = player[1]
             else:
                 player_game.image = player[0]
---
+++ src/sprites.py
@@ -0,0 +1,56 @@
+import random
+import pygame
+from settings import HEIGHT, WIDTH, gnome, player, beer
+
+class Gnome:
+    def __init__(self):
+        self.image = gnome
+        self.width = 128
+        self.height = 128
+        self.walk = 0
+        self.y = 0
+        self.x = random.randrange(WIDTH - self.width)
+        self.speedx = random.randrange(-8, 8)
+        self.speedy = random.randrange(3, 8)
+
+    def draw(self, screen):
+        screen.blit(self.image[self.walk], (self.x, self.y))
+
+
+class Player:
+    def __init__(self):
+        self.image = player[0]
+        self.width = self.image.get_width()
+        self.height = self.image.get_height()
+        self.y = HEIGHT - self.height
+        self.x = WIDTH // 2.5
+        self.speedx = 10
+
+    def movement(self):
+        keystate = pygame.key.get_pressed()
+        if keystate[pygame.K_LEFT]:
+            self.x -= self.speedx
+        if keystate[pygame.K_RIGHT]:
+            self.x += self.speedx
+        if self.x < 0:
+            self.x = 0
+        if self.x > WIDTH - self.width:
+            self.x = WIDTH - self.width
+
+    def draw(self, screen):
+        screen.blit(self.image, (self.x, self.y))
+
+
+class Projectile:
+    def __init__(self, x, y):
+        self.image = beer
+        self.x, self.y = x, y
+        self.width = self.image.get_width()
+        self.height = self.image.get_height()
+        self.speedy = 10
+
+    def beer_cast(self):
+        self.y -= self.speedy
+
+    def draw(self, screen):
+        screen.blit(self.image, (self.x, self.y))
